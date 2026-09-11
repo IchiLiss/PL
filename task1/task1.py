6a0475d37cad3c485b7b920cb8cb1806cc8bcf2a
IchiLiss <irushka1307@gmail.com>
2026-09-11 17:13:11 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -11,19 +11,14 @@ def get_path(n, m):
     return path
 
 def main():
-    default = [6, 3, 5, 4]
-
-    args = sys.argv[1:]
-    if len(args) == 4:
-        nums = list(map(int, args))
-    else:
-        print("Аргументы не переданы, используются значения по умолчанию:", *default)
-        nums = default
-
-    n1, m1, n2, m2 = nums
+    if len(sys.argv) != 5:
+        print("Usage: task1.py n1 m1 n2 m2")
+        return
+    n1, m1, n2, m2 = map(int, sys.argv[1:5])
     path1 = get_path(n1, m1)
     path2 = get_path(n2, m2)
-    print(''.join(map(str, path1 + path2)))
+    result = ''.join(map(str, path1 + path2))
+    print(result)
 
 if __name__ == "__main__":
     main()
